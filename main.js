fdd2b7266f08b22ffd2ebe7715007b9c328e4f63
Aashish Jalan <77002032+Aashish-Jalan@users.noreply.github.com>
2021-08-11 16:56:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
-
 function addUser() {
-  user_name = document.getElementById("user_name").value;
-  firebase.database().ref("/").child(user_name).update({
-      purpose:"adding user"
-  });
+
+    user_name = document.getElementById("user_name").value;
+
+    localStorage.setItem("user_name" , user_name)
 
       window.location = "chat.html";
 }
+
